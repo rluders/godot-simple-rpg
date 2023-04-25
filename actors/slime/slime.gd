f64ea4db8348ce18d73272b34bbd2aa5b8262530
Ricardo Lüders <rluders@redhat.com>
2023-04-25 16:24:14 +0200

    feat: adding the slime enemy

---
+++ actors/slime/slime.gd
@@ -0,0 +1,27 @@
+extends Actor
+
+
+var chase_target : Node2D
+
+@onready var sprite : Sprite2D = $Sprite2D
+
+
+func _physics_process(delta: float) -> void:
+	if chase_target:
+		anim_state.travel("Walk")
+		var direction : = (chase_target.global_position - global_position).normalized()
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+		sprite.flip_h = velocity.x < 0
+	else:
+		anim_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide()
+
+
+func _on_detection_area_target_found(target) -> void:
+	chase_target = target
+
+
+func _on_detection_area_target_lost(target) -> void:
+	chase_target = null
